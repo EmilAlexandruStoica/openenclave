c69e1b8a095d20fd27182458890d2a6cb9dee16b
Emil Alexandru Stoica <astoica@cloudbasesolutions.com>
2019-04-16 02:22:01 -0700

    Add new sample

---
+++ samples/randomSample/host/host.c
@@ -0,0 +1,78 @@
+#include <openenclave/host.h>
+#include <stdio.h>
+#include "randomSample_u.h"
+
+bool check_simulate_opt(int* argc, const char* argv[])
+{
+    for (int i = 0; i < *argc; i++)
+    {
+        if (strcmp(argv[i], "--simulate") == 0)
+        {
+            fprintf(stdout, "Running in simulation mode\n");
+            memmove(&argv[i], &argv[i + 1], (*argc - i) * sizeof(char*));
+            (*argc)--;
+            return true;
+        }
+    }
+    return false;
+}
+
+int host_randomSample()
+{
+    return 10;
+}
+
+int main(int argc, const char* argv[])
+{
+    oe_result_t result;
+    int value = host_randomSample();
+    int ret = 1;
+    oe_enclave_t* enclave = NULL;
+
+    uint32_t flags = OE_ENCLAVE_FLAG_DEBUG;
+    if (check_simulate_opt(&argc, argv))
+    {
+        flags |= OE_ENCLAVE_FLAG_SIMULATE;
+    }
+
+    if (argc != 2)
+    {
+        fprintf(
+            stderr, "Usage: %s enclave_image_path [ --simulate  ]\n", argv[0]);
+        goto exit;
+    }
+
+    // Create the enclave
+    result = oe_create_randomSample_enclave(
+        argv[1], OE_ENCLAVE_TYPE_AUTO, flags, NULL, 0, &enclave);
+    if (result != OE_OK)
+    {
+        fprintf(
+            stderr,
+            "oe_create_randomSample_enclave(): result=%u (%s)\n",
+            result,
+            oe_result_str(result));
+        goto exit;
+    }
+
+    // Call into the enclave
+    value = enclave_randomSample(enclave, &value);
+    if (value != 0)
+    {
+        fprintf(
+            stderr,
+            "calling into enclave_randomSample failed: result=%u (%s)\n",
+            result,
+            oe_result_str(result));
+        goto exit;
+    }
+
+    ret = 0;
+
+exit:
+    // Clean up the enclave if we created one
+    if (enclave)
+        oe_terminate_enclave(enclave);
+
+    return ret;
+}
